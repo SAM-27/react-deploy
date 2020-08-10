ae815cad27fb0cc6dc665a24ab460e29cb3b6891
SAM-27 <48250127+SAM-27@users.noreply.github.com>
2020-08-10 14:20:53 +0530

    Updates

---
+++ precache-manifest.d9b54570df6b96b603ff103a5356eda9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ab99696ecdca9395c5bc403eab44b52",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "5709f173337630473c46",
+    "url": "/react-deploy/static/css/main.4d4cc984.chunk.css"
+  },
+  {
+    "revision": "6a10a78df6c76ca3a627",
+    "url": "/react-deploy/static/js/2.b1b31c97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.b1b31c97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5709f173337630473c46",
+    "url": "/react-deploy/static/js/main.ac9d8716.chunk.js"
+  },
+  {
+    "revision": "9a6b4006b58ad85b50f6",
+    "url": "/react-deploy/static/js/runtime-main.3e11c617.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
