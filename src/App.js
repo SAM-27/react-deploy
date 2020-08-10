acc83807e9f3d9a42f9831e5a63cba501c77a664
SAM-27 <48250127+SAM-27@users.noreply.github.com>
2020-08-10 14:18:33 +0530

    deploy my react app to github pages

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>My react app in github pages</h2>
+       <h3>Done something new</h3>      
+    </div>
+  );
+}
+
+export default App;
